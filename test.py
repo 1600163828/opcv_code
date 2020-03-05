1c9918b59d52052f0e921a1d3e5bef200e41ff35
杨康 <1600163828@qq.com>
2020-03-05 09:33:38 +0800

    添加了一个文件

--- test.py
+++ test.py
@@ -1,3 +1,9 @@
-print("mksmks")
+import cv2 as cv
 
+imread = cv.imread("D:/Users/admin/Desktop/opcv_code/image/pycharm_git_013.jpg")
 
+cv.namedWindow("图片",cv.WINDOW_AUTOSIZE)
+cv.imshow("图片",imread)
+print('图片')
+cv.waitKey(0)
+cv.destroyAllWindows()
